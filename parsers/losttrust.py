0a9bf3cd78291231e736f5926522705c0e6c5fea
Julien Mousqueton <julien@mousqueton.io>
2023-09-26 13:19:58 +0200

    Create new parser for losttrust issue : https://github.com/JMousqueton/ransomware.live/issues/45

---
+++ parsers/losttrust.py
@@ -0,0 +1,35 @@
+
+"""
++------------------------------+------------------+----------+
+| Description | Published Date | Victim's Website | Post URL |
++------------------------------+------------------+----------+
+|      X      |                | include in desc  |     x    |
++------------------------------+------------------+----------+
+Rappel : def appender(post_title, group_name, description="", website="", published="", post_url=""):
+"""
+import os
+from bs4 import BeautifulSoup
+from sharedutils import stdlog, errlog
+from parse import appender
+
+
+def main():
+    for filename in os.listdir('source'):
+        try:
+            if filename.startswith('losttrust-'):
+                html_doc='source/'+filename
+                file=open(html_doc,'r')
+                soup=BeautifulSoup(file,'html.parser')
+                companies = soup.find_all('div', class_='card')
+                for company in companies:
+                    try: 
+                        victim = company.find('div', class_='card-header').text.strip()
+                    except: 
+                        continue
+                    description = company.find('div', class_='card-body').p.text.strip()
+                    website = company.find('a', href=True, text='Visit site')['href']
+                    appender(victim, 'losttrust', description.replace('\n',' '),"https://"+website)
+                file.close()
+        except:
+            errlog('losttrust: ' + 'parsing fail')
+            pass    
