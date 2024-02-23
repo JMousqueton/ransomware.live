c2cbc57a2c195a2b908527627e9223f90084e49e
Julien Mousqueton <julien@mousqueton.io>
2024-02-23 19:22:16 +0100

    optimizationssss

--- check_url_in_ransomnotes.py
+++ check_url_in_ransomnotes.py
@@ -17,24 +17,33 @@ json_file_path = './groups.json'
 # Add the exception URLs here
 exception_urls = [
     'torpastezr7464pevuvdjisbvaf4yqi4n7sgz7lkwgqwxznwy5duj4ad.onion',
-    'myosbja7hixkkjqihsjh6yvmqplz62gr3r4isctjjtu2vm5jg6hsv2ad.onion',  
-    'ragnarmj3hlykxstyanwtgf33eyacccleg45ctygkuw7dkgysict6xyd.onion', # Seized by Police ;) 
-    'j3qxmk6g5sk3zw62i2yhjnwmhm55rfz47fdyfkhaithlpelfjdokdxad.onion', # White page 
-    'monti5o7lvyrpyk26lqofnfvajtyqruwatlfaazgm3zskt3xiktudwid.onion' #Page not Found 
+    'myosbja7hixkkjqihsjh6yvmqplz62gr3r4isctjjtu2vm5jg6hsv2ad.onion',
+    'ragnarmj3hlykxstyanwtgf33eyacccleg45ctygkuw7dkgysict6xyd.onion',  # Seized by Police ;)
+    'j3qxmk6g5sk3zw62i2yhjnwmhm55rfz47fdyfkhaithlpelfjdokdxad.onion',  # White page
+    'monti5o7lvyrpyk26lqofnfvajtyqruwatlfaazgm3zskt3xiktudwid.onion'  # Page not Found
 ]
 
+
 def find_onion_urls(directory):
-    onion_urls = set()
+    onion_urls = {}  # Changed to a dictionary
     onion_pattern = re.compile(r'https?://[a-z2-7]{56}\.onion\b|\b[a-z2-7]{56}\.onion\b')
     for root, _, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
+            # Split the file path and extract the last subdirectory name
+            subdirectory_name = os.path.basename(os.path.dirname(file_path))
             with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
                 content = f.read()
                 matches = onion_pattern.findall(content)
                 normalized_matches = [re.sub(r'^https?://', '', url) for url in matches]
-                onion_urls.update(normalized_matches)
+                for url in normalized_matches:
+                    if url in onion_urls:
+                        onion_urls[url].append(subdirectory_name)  # Add the subdirectory name instead of the full path
+                    else:
+                        onion_urls[url] = [subdirectory_name]
     return onion_urls
+
+
 
 def get_fqdn_from_json(json_file_path):
     fqdn_set = set()
@@ -48,35 +57,23 @@ def get_fqdn_from_json(json_file_path):
                     fqdn_set.add(fqdn)
     return fqdn_set
 
-def check_onion_urls_online(onion_urls, exception_urls):
+def check_onion_urls_online(onion_urls_dict, exception_urls):
     session = requests.Session()
     session.proxies = {
         'http': 'socks5h://localhost:9050',
         'https': 'socks5h://localhost:9050',
     }
 
-    online_urls = []
-    for url in onion_urls:
+    online_urls = {}
+    for url, source_files in onion_urls_dict.items():
         if url in exception_urls:
-            # print(f'Skipping exception URL: {url}')
-            continue  # Skip this URL as it's in the exception list
+            continue
         try:
             response = session.get(f'http://{url}', timeout=10)
             if response.status_code == 200:
-                online_urls.append(url)
-                print({url})
-            else:
-                pass
-                # print(f'Offline or Error: {url} - Status Code: {response.status_code}')
-        except requests.exceptions.ConnectTimeout:
+                online_urls[url] = source_files
+        except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
             pass
-            #print(f'Connection Timeout: {url} - The site may be offline or unreachable.')
-        except requests.exceptions.ConnectionError:
-            # Silencing the ConnectionError
-            pass
-        except Exception as e:
-            pass
-            # print(f'Error connecting to {url}: {e}')
     return online_urls
 
 print(
@@ -98,12 +95,16 @@ print(
 
 
 # Process
-onion_urls = find_onion_urls(directory_to_search)
+onion_urls_dict = find_onion_urls(directory_to_search)
 fqdn_set = get_fqdn_from_json(json_file_path)
-missing_urls = onion_urls - fqdn_set
 
-if missing_urls:
+# Identifying missing URLs and checking their online status
+missing_urls_dict = {url: files for url, files in onion_urls_dict.items() if url not in fqdn_set}
+if missing_urls_dict:
     stdlog("Checking online status for .onion URLs not present in groups.json...")
-    online_urls = check_onion_urls_online(missing_urls, exception_urls)
+    online_urls = check_onion_urls_online(missing_urls_dict, exception_urls)
+    for url, source_subdirectories in online_urls.items():
+        source_files_str = f'[{source_subdirectories[0]}]' if source_subdirectories else '[Unknown]'
+        print(f'{source_files_str} {url}')
 else:
     stdlog("No new .onion URLs found outside of groups.json.")
