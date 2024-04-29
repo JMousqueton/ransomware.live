00c102a4e853a1e6bbd74f23156d9e1cbbb153ad
Julien Mousqueton <julien@mousqueton.io>
2024-04-29 09:01:43 +0200

    Update ATP73 for victim with unpublished data

--- parsers/eraleig.py
+++ parsers/eraleig.py
@@ -20,12 +20,13 @@ def main():
                 file=open(html_doc,'r')
                 soup = BeautifulSoup(file, 'html.parser')
                 segment_box = soup.find('div', class_='segment__box')
-                for segment in segment_box.find_all('div', class_='segment published'):
-                    post_url = find_slug_by_md5('eraleig', extract_md5_from_filename(html_doc)) + segment.get('onclick').split('=')[1].strip("'")+"="+segment.get('onclick').split('=')[2].strip("'") 
-                    victim = segment.find('div', class_='segment__text__off').text.strip()
-                    description = segment.find('div', class_='segment__text__dsc').text.strip()
-                    date_text = segment.find('div', class_='segment__date__deadline').text.strip()
-                    date_text = date_text.replace('UTC +0', '').strip()
+                for segment in segment_box.find_all('div', class_='segment'):
+                    link = segment.get('onclick').split("'")[1] if segment.get('onclick') else None
+                    victim = segment.find('div', class_='segment__text__off').text.strip() if segment.find('div', class_='segment__text__off') else None
+                    post_url = 'http://wn6vonooq6fggjdgyocp7bioykmfjket7sbp47cwhgubvowwd7ws5pyd.onion' +  str(link)
+                    description = segment.find('div', class_='segment__text__dsc').text.strip() if segment.find('div', class_='segment__text__dsc') else None
+                    date_info = segment.find('div', class_='segment__date__deadline').text.strip() if segment.find('div', class_='segment__date__deadline') else None
+                    date_text = date_info.replace('UTC +0', '').strip()
                     date_obj = datetime.strptime(date_text, '%Y/%m/%d %H:%M:%S')
                     date = date_obj.strftime('%Y-%m-%d %H:%M:%S.%f')
                     appender(victim,'eraleig',description,'',date,post_url)
