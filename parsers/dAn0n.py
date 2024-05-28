6e9008b75083ecca843ec369089cdeec1e234d1c
Julien Mousqueton <julien@mousqueton.io>
2024-05-28 23:29:11 +0200

    Update parser for dAn0n

--- parsers/dAn0n.py
+++ parsers/dAn0n.py
@@ -25,14 +25,15 @@ def main():
                 cards = soup.find_all('div', class_='card mb-3')
                 # Extracting information from each card
                 for card in cards:
-                    title = card.find('h5', class_='card-title').get_text(strip=True)
-                    date_str = card.find('p', class_='card-text text-muted h6').get_text(strip=True)
+                    title = card.find('h6', class_='card-title').get_text(strip=True)
+                    date_str = card.find('p', class_='card-text h6').get_text(strip=True)
                     date = datetime.strptime(date_str, '%b %d, %Y')
                     formatted_date = date.strftime('%Y-%m-%d %H:%M:%S.000000')
-                    #description = card.find_all('p', class_='card-text')[1].text.strip()
+                    description = card.find('p', class_='card-text text-muted').text.strip() #[&]
+                    print(description)
                     link = card.find('a', class_='btn btn-primary')['href']
                     if link: 
                         link=find_slug_by_md5('dAn0n', extract_md5_from_filename(html_doc)) +  link
-                    appender(title,'dAn0n', '', '',formatted_date,link)
+                    appender(title,'dAn0n', description, '',formatted_date,link)
         except Exception as e:
             errlog('dAn0n - parsing fail with error: ' + str(e))
